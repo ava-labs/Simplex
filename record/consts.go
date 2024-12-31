d850582cdec0c30676e1976eb3c713c7a21f5d1a
Yacov Manevich <yacov.manevich@avalabs.org>
2024-12-31 21:24:59 +0100

    Remove unused code
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>

--- record/consts.go
+++ record/consts.go
@@ -3,17 +3,9 @@
 
 package record
 
-
 const (
 	UndefinedRecordType uint16 = iota
 	BlockRecordType
 	NotarizationRecordType
 	FinalizationRecordType
-
-	recordChecksumLen = 8
-	recordSizeLen     = 4
-	recordVersionLen  = 1
-	recordTypeLen     = 2
-
-	maxBlockSize = 100_000_000 // ~ 100MB
 )
