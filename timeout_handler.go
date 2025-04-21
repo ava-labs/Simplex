f9aa00238fe38c122aaf83e27f7757cc392db5a0
samliok <samliok@stanford.edu>
2025-04-20 22:28:05 -0700

    remove goto label

--- timeout_handler.go
+++ timeout_handler.go
@@ -170,18 +170,15 @@ func (t *TimeoutHandler) Close() {
 // FindTask returns the first TimeoutTask assigned to [node] that contains any sequence in [seqs].
 // A sequence is considered "contained" if it falls between a task's Start (inclusive) and End (inclusive).
 func (t *TimeoutHandler) FindTask(node NodeID, seqs []uint64) *TimeoutTask {
-	var task *TimeoutTask
-seq:
 	for _, seq := range seqs {
 		for _, t := range t.tasks[string(node)] {
 			if seq >= t.Start && seq <= t.End {
-				task = t
-				break seq
+				return t
 			}
 		}
 	}
 
-	return task
+	return nil
 }
 
 const delimiter = "_"
