9d780972fe11c78594fbd9f1e0260510e20256c9
samliok <samliok@stanford.edu>
2025-07-11 16:14:59 -0400

    add proper test

--- replication_timeout_test.go
+++ replication_timeout_test.go
@@ -4,7 +4,9 @@
 package simplex_test
 
 import (
+	"bytes"
 	"testing"
+	"time"
 
 	"github.com/ava-labs/simplex"
 
@@ -308,67 +310,71 @@ func TestReplicationRequestIncompleteResponses(t *testing.T) {
 // if we are expecting a finalization but it is not present in the response.
 func TestReplicationRequestWithoutFinalization(t *testing.T) {
 	nodes := []simplex.NodeID{{1}, {2}, {3}, []byte("lagging")}
-	startSeq := uint64(8)
-
-	// node begins replication
+	endDisconnect := uint64(10)
 	bb := newTestControlledBlockBuilder(t)
+	laggingBb := newTestControlledBlockBuilder(t)
 	net := newInMemNetwork(t, nodes)
-
-	storageData := createBlocks(t, nodes, &bb.testBlockBuilder, startSeq)
-
-	newNodeConfig := func(from simplex.NodeID) *testNodeConfig {
-		comm := newTestComm(from, net, rejectReplicationRequests)
-		return &testNodeConfig{
-			initialStorage:     storageData,
-			comm:               comm,
-			replicationEnabled: true,
-		}
-	}
-
-	mf := &testTimeoutMessageFilter{
-		t:                    t,
-		replicationResponses: make(chan struct{}, 1),
-	}
-
-	newSimplexNode(t, nodes[0], net, bb, newNodeConfig(nodes[0]))
-	normalNode2 := newSimplexNode(t, nodes[1], net, bb, newNodeConfig(nodes[1]))
-	normalNode2.e.Comm.(*testComm).setFilter(mf.receivedReplicationRequest)
-	newSimplexNode(t, nodes[2], net, bb, newNodeConfig(nodes[2]))
-	laggingNode := newSimplexNode(t, nodes[3], net, bb, &testNodeConfig{
+	testConfig := &testNodeConfig{
 		replicationEnabled: true,
-	})
+	}
+	newSimplexNode(t, nodes[0], net, bb, testConfig)
+	newSimplexNode(t, nodes[1], net, bb, testConfig)
+	newSimplexNode(t, nodes[2], net, bb, testConfig)
+	laggingNode := newSimplexNode(t, nodes[3], net, laggingBb, testConfig)
 
+	epochTimes := make([]time.Time, 0, 4)
+	for _, n := range net.instances {
+		epochTimes = append(epochTimes, n.e.StartTime)
+	}
+	// lagging node disconnects
+	net.Disconnect(nodes[3])
 	net.startInstances()
-	bb.triggerNewBlock()
 
-	// typically the lagging node would catch up here, but since we block
-	// replication requests, the lagging node will be forced to resend requests after a timeout
-	for i := 0; i <= int(startSeq); i++ {
-		for _, n := range net.instances {
-			if n.e.ID.Equals(laggingNode.e.ID) {
-				continue
+	missedSeqs := uint64(0)
+	// normal nodes continue to make progress
+	for i := uint64(0); i < endDisconnect; i++ {
+		emptyRound := bytes.Equal(simplex.LeaderForRound(nodes, i), nodes[3])
+		if emptyRound {
+			advanceWithoutLeader(t, net, bb, epochTimes, i, laggingNode.e.ID)
+			missedSeqs++
+		} else {
+			bb.triggerNewBlock()
+			for _, n := range net.instances[:3] {
+				n.storage.waitForBlockCommit(i - missedSeqs)
 			}
-			n.storage.waitForBlockCommit(uint64(startSeq))
 		}
 	}
-
-	// this is done from normalNode2 since the lagging node will request
-	// seqs [0-startSeq/3] after the timeout
-	<-mf.replicationResponses
-
-	// assert the lagging node has not received any replication responses
+	// all nodes excpet for lagging node have progressed and commited [endDisconnect - missedSeqs] blocks
+	for _, n := range net.instances[:3] {
+		require.Equal(t, endDisconnect-missedSeqs, n.storage.Height())
+	}
 	require.Equal(t, uint64(0), laggingNode.storage.Height())
+	require.Equal(t, uint64(0), laggingNode.e.Metadata().Round)
+	// lagging node reconnects
 	net.setAllNodesMessageFilter(removeFinalizationsFromReplicationResponses)
+	net.Connect(nodes[3])
+	bb.triggerNewBlock()
+	for _, n := range net.instances {
+		if n.e.ID.Equals(laggingNode.e.ID) {
+			continue
+		}
+		n.storage.waitForBlockCommit(endDisconnect - missedSeqs)
+	}
 
-	// after the timeout, only normalNode2 should respond(but with incomplete data)
-	laggingNode.e.AdvanceTime(laggingNode.e.StartTime.Add(simplex.DefaultReplicationRequestTimeout / 2))
+	// wait until the lagging nodes sends replication requests
+	time.Sleep(100 * time.Millisecond)
+	// due to the removeFinalizationsFromReplicationResponses message filter
+	// the lagging node should not have processed any replication requests
 	require.Equal(t, uint64(0), laggingNode.storage.Height())
 
-	laggingNode.e.AdvanceTime(laggingNode.e.StartTime.Add(simplex.DefaultReplicationRequestTimeout))
-	laggingNode.storage.waitForBlockCommit(0)
-
-	net.setAllNodesMessageFilter(allowAllMessages)
-	// timeout again, now all nodes will respond
+	// we should still have these replication requests in the timeout handler
 	laggingNode.e.AdvanceTime(laggingNode.e.StartTime.Add(simplex.DefaultReplicationRequestTimeout * 2))
-	laggingNode.storage.waitForBlockCommit(startSeq)
+	time.Sleep(100 * time.Millisecond) // wait for the tick
+	require.Equal(t, uint64(0), laggingNode.storage.Height())
+
+	// We should still have these replication requests in the timeout handler
+	// but now we allow the lagging node to process them
+	net.setAllNodesMessageFilter(allowAllMessages)
+	laggingNode.e.AdvanceTime(laggingNode.e.StartTime.Add(simplex.DefaultReplicationRequestTimeout * 4))
+	laggingNode.storage.waitForBlockCommit(endDisconnect - missedSeqs)
 }
