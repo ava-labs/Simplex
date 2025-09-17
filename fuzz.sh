f3c25f85fdf62d3eb9663f7c126f736e04a6a7b1
Yacov Manevich <yacov.manevich@avalabs.org>
2025-09-18 01:34:02 +0200

    Skip fuzz tests in most CI runs
    This commit makes the fuzz tests be skipped in 4 out of 5 runs.
    
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>

--- fuzz.sh
+++ fuzz.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env -euxo pipefail bash
 
+rand=$RANDOM
+x=$(( $rand % 5 ))
+if [[ $x -ne 0 ]]; then
+  echo "Skipping fuzz run"
+  exit 0
+fi
+
 grep -ore "Fuzz.*(f \*" * | cut -d\( -f1 | while read line; do
   file=$( echo $line | cut -d: -f1 )
   testName=$( echo $line | cut -d: -f2  )
