e96ac60cf6e6284a86a9679cad4281fcf2e6b0dc
samliok <samliok@stanford.edu>
2025-09-24 13:31:50 -0400

    nit

--- epoch.go
+++ epoch.go
@@ -2629,6 +2629,7 @@ func (e *Epoch) processReplicationState() error {
 	qRound, ok := e.replicationState.receivedQuorumRounds[e.round]
 	if ok && qRound.Notarization != nil {
 		if qRound.Finalization != nil {
+			e.Logger.Debug("Delaying processing a QuorumRound that has an Finalization != NextSeqToCommit", zap.Stringer("QuorumRound", &qRound))
 			return nil
 		}
 		delete(e.replicationState.receivedQuorumRounds, e.round)
