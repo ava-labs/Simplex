79209d9f83d3e166d802bb712e92dca505b09838
samliok <samliok@stanford.edu>
2025-11-05 12:31:15 -0600

    remove bug test file

--- epoch_test.go
+++ epoch_test.go
@@ -968,6 +968,108 @@ func TestMetadataProposedRound(t *testing.T) {
 	require.Zero(t, e.Metadata().Seq)
 }
 
+// TestFinalizeSameSequence tests that a node only verifies a block after it receives all the empty notarizations the block depends on.
+// This means the node can(and should) send a finalize vote for the same sequence if and only if a valid empty notarization has been verified.
+//
+// The test does this
+// Round 0 := finalized block of seq 0
+// Round 1 := receive block of seq 1, round 1 advance from notarization & send finalize vote
+// Round 2 := receive block of seq 1, round 2 and are unable to verify(because we don't have empty notarization yet)
+//
+// Once we receive the empty notarization, we can verify the block(seq 1, round 2) and send out a finalize vote
+func TestFinalizeSameSequence(t *testing.T) {
+	bb := &testutil.TestBlockBuilder{Out: make(chan *testutil.TestBlock, 1), BlockShouldBeBuilt: make(chan struct{}, 1)}
+	ctx := context.Background()
+	nodes := []NodeID{{1}, {2}, {3}, {4}}
+	initialBlock := createBlocks(t, nodes, 1)[0]
+	recordingComm := &recordingComm{Communication: testutil.NewNoopComm(nodes), BroadcastMessages: make(chan *Message, 100), SentMessages: make(chan *Message, 100)}
+	conf, wal, storage := testutil.DefaultTestNodeEpochConfig(t, nodes[0], recordingComm, bb)
+	storage.Index(ctx, initialBlock.VerifiedBlock, initialBlock.Finalization)
+
+	e, err := NewEpoch(conf)
+	require.NoError(t, err)
+
+	require.NoError(t, e.Start())
+	require.Equal(t, uint64(1), e.Metadata().Seq)
+
+	// we receive a block and then notarize(this sends out a finalize vote for the block)
+	advanceRoundFromNotarization(t, e, bb)
+	require.Equal(t, uint64(2), e.Metadata().Seq)
+	require.Equal(t, uint64(2), e.Metadata().Round)
+
+	// wait for finalize votes
+	for {
+		msg := <-recordingComm.BroadcastMessages
+		if msg.FinalizeVote != nil {
+			require.Equal(t, uint64(1), msg.FinalizeVote.Finalization.Round)
+			require.Equal(t, uint64(1), msg.FinalizeVote.Finalization.Seq)
+			break
+		}
+	}
+
+	// clear the recorded messages
+	for len(recordingComm.BroadcastMessages) > 0 {
+		<-recordingComm.BroadcastMessages
+	}
+
+	md := ProtocolMetadata{
+		Round: 2,
+		Seq:   1, // set next seq to 1 not 2
+		Prev:  initialBlock.VerifiedBlock.BlockHeader().Digest,
+	}
+	_, ok := bb.BuildBlock(context.Background(), md, simplex.Blacklist{
+		NodeCount: uint16(len(e.EpochConfig.Comm.Nodes())),
+	})
+	require.True(t, ok)
+
+	block := <-bb.Out
+	block.OnVerify = func() {
+		require.Fail(t, "block should not be verified since we don't have empty notarization for round 1")
+	}
+
+	// send block from leader
+	vote, err := testutil.NewTestVote(block, nodes[2])
+	require.NoError(t, err)
+	err = e.HandleMessage(&simplex.Message{
+		BlockMessage: &simplex.BlockMessage{
+			Vote:  *vote,
+			Block: block,
+		},
+	}, nodes[2])
+	require.NoError(t, err)
+
+	// give some time for block to be (not)verified
+	time.Sleep(100 * time.Millisecond)
+	block.OnVerify = nil
+
+	// now lets send empty notarization
+	emptyNotarization := testutil.NewEmptyNotarization(nodes, 1)
+	err = e.HandleMessage(&simplex.Message{
+		EmptyNotarization: emptyNotarization,
+	}, nodes[3])
+	require.NoError(t, err)
+
+	// create a notarization and now we should send a finalize vote for seq 1 again
+	notarization, err := testutil.NewNotarization(e.Logger, e.SignatureAggregator, block, nodes[1:])
+	require.NoError(t, err)
+	testutil.InjectTestNotarization(t, e, notarization, nodes[1])
+
+	wal.AssertNotarization(block.Metadata.Round)
+
+	// wait for finalize votes
+	for {
+		msg := <-recordingComm.BroadcastMessages
+		if msg.FinalizeVote != nil {
+			require.Equal(t, uint64(2), msg.FinalizeVote.Finalization.Round)
+			require.Equal(t, uint64(1), msg.FinalizeVote.Finalization.Seq)
+			break
+		}
+	}
+
+	require.Equal(t, uint64(2), e.Metadata().Seq)
+	require.Equal(t, uint64(3), e.Metadata().Round)
+}
+
 func TestEpochVotesForEquivocatedVotes(t *testing.T) {
 	bb := &testutil.TestBlockBuilder{Out: make(chan *testutil.TestBlock, 1)}
 
