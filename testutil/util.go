519bef62a5a459bab1e7e2b911deca23ce2494c0
samliok <samliok@stanford.edu>
2025-12-02 18:01:55 -0500

    old todo

--- testutil/util.go
+++ testutil/util.go
@@ -207,7 +207,6 @@ func WaitForBlockProposerTimeout(t *testing.T, e *simplex.Epoch, startTime *time
 		}
 
 		// if we are expected to time out for this round, we should not have a notarization
-		// TODO: this line is breaking TestSimplexMultiNodeBlacklist but i do not know why yet
 		require.False(t, e.WAL.(*TestWAL).ContainsNotarization(startRound), fmt.Sprintf("should not have notarized %d for node %s", startRound, e.ID))
 
 		*startTime = startTime.Add(e.EpochConfig.MaxProposalWait / 5)
