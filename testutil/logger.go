2864f13a5a8b997b076ed082058e73ce91d3b70c
Sam Liokumovich <65994425+samliok@users.noreply.github.com>
2026-02-05 15:01:50 -0500

    Add Test Name to Logger Panic (#333)
    * add test name to logger
    
    * rename to tl

--- testutil/logger.go
+++ testutil/logger.go
@@ -16,6 +16,7 @@ import (
 
 type TestLogger struct {
 	*zap.Logger
+	t                  *testing.T
 	traceVerboseLogger *zap.Logger
 	panicOnError       bool
 	panicOnWarn        bool
@@ -39,36 +40,36 @@ func (k keywordFilterCore) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *z
 	return ce
 }
 
-func (t *TestLogger) Intercept(hook func(entry zapcore.Entry) error) {
-	logger := t.Logger.WithOptions(zap.Hooks(hook))
-	t.Logger = logger
+func (tl *TestLogger) Intercept(hook func(entry zapcore.Entry) error) {
+	logger := tl.Logger.WithOptions(zap.Hooks(hook))
+	tl.Logger = logger
 }
 
-func (t *TestLogger) Silence() {
+func (tl *TestLogger) Silence() {
 	atomicLevel := zap.NewAtomicLevelAt(zapcore.FatalLevel)
-	core := t.Logger.Core()
-	t.Logger = zap.New(core, zap.AddCaller(), zap.IncreaseLevel(atomicLevel))
-	t.traceVerboseLogger = zap.New(core, zap.AddCaller(), zap.IncreaseLevel(atomicLevel))
+	core := tl.Logger.Core()
+	tl.Logger = zap.New(core, zap.AddCaller(), zap.IncreaseLevel(atomicLevel))
+	tl.traceVerboseLogger = zap.New(core, zap.AddCaller(), zap.IncreaseLevel(atomicLevel))
 }
 
 // SilenceExceptKeywords silences all logs EXCEPT those whose message contains
 // at least one of the provided keywords.
-func (t *TestLogger) SilenceExceptKeywords(keywords ...string) {
-	core := t.Logger.Core()
+func (tl *TestLogger) SilenceExceptKeywords(keywords ...string) {
+	core := tl.Logger.Core()
 	filteredCore := keywordFilterCore{
 		Core:     core,
 		keywords: keywords,
 	}
-	t.Logger = zap.New(filteredCore, zap.AddCaller())
-	t.traceVerboseLogger = zap.New(filteredCore, zap.AddCaller())
+	tl.Logger = zap.New(filteredCore, zap.AddCaller())
+	tl.traceVerboseLogger = zap.New(filteredCore, zap.AddCaller())
 }
 
-func (t *TestLogger) SetPanicOnError(panicOnError bool) {
-	t.panicOnError = panicOnError
+func (tl *TestLogger) SetPanicOnError(panicOnError bool) {
+	tl.panicOnError = panicOnError
 }
 
-func (t *TestLogger) SetPanicOnWarn(panicOnWarn bool) {
-	t.panicOnWarn = panicOnWarn
+func (tl *TestLogger) SetPanicOnWarn(panicOnWarn bool) {
+	tl.panicOnWarn = panicOnWarn
 }
 
 func (tl *TestLogger) Trace(msg string, fields ...zap.Field) {
@@ -82,7 +83,7 @@ func (tl *TestLogger) Verbo(msg string, fields ...zap.Field) {
 func (tl *TestLogger) Warn(msg string, fields ...zap.Field) {
 	tl.Logger.Warn(msg, fields...)
 	if tl.panicOnWarn {
-		panicMsg := fmt.Sprintf("WARN: %s", msg)
+		panicMsg := fmt.Sprintf("WARN during test %s: %s", tl.t.Name(), msg)
 		panic(panicMsg)
 	}
 }
@@ -90,7 +91,7 @@ func (tl *TestLogger) Warn(msg string, fields ...zap.Field) {
 func (tl *TestLogger) Error(msg string, fields ...zap.Field) {
 	tl.Logger.Error(msg, fields...)
 	if tl.panicOnError {
-		panicMsg := fmt.Sprintf("ERROR: %s", msg)
+		panicMsg := fmt.Sprintf("ERROR during test %s: %s", tl.t.Name(), msg)
 		panic(panicMsg)
 	}
 }
@@ -157,7 +158,7 @@ func MakeLoggerWithFile(t *testing.T, fileWriter zapcore.WriteSyncer, node ...in
 		traceVerboseLogger = traceVerboseLogger.With(zap.Int("myNodeID", node[0]))
 	}
 
-	l := &TestLogger{Logger: logger, traceVerboseLogger: traceVerboseLogger,
+	l := &TestLogger{t: t, Logger: logger, traceVerboseLogger: traceVerboseLogger,
 		atomicLevel: atomicLevel,
 	}
 
@@ -185,6 +186,6 @@ func (dse *DebugSwallowingEncoder) EncodeEntry(entry zapcore.Entry, fields []zap
 	return dse.consoleEncoder.EncodeEntry(entry, fields)
 }
 
-func (t *TestLogger) SetLevel(level zapcore.Level) {
-	t.atomicLevel.SetLevel(level)
+func (tl *TestLogger) SetLevel(level zapcore.Level) {
+	tl.atomicLevel.SetLevel(level)
 }
