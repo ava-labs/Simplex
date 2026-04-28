f35b3a2b3bf912709f014002e2a295967416f121
Sam Liokumovich <65994425+samliok@users.noreply.github.com>
2026-04-28 15:30:29 +0300

    log diverge in debug (#371)

--- replication_test.go
+++ replication_test.go
@@ -696,6 +696,8 @@ func TestReplicationStuckInProposingBlock(t *testing.T) {
 // have a stale notarization for a round(i.e. a node notarized a block but the rest of the network
 // propagated an empty notarization).
 func TestReplicationNodeDiverges(t *testing.T) {
+	t.Setenv("LOG_LEVEL", "debug")
+
 	nodes := []simplex.NodeID{{1}, {2}, {3}, {4}, {5}, {6}}
 	numBlocks := uint64(5)
 
