181f777f1e8beb59bda2f885cbdd3ffd0202d871
yacovm <yacovm@users.noreply.github.com>
2026-07-09 18:26:23 +0200

    Abort block building and sending upon commit of an epoch sealing block (#433)
    * Abort block building and sending upon commit of an epoch sealing block
    
    Once a block carrying SealingBlockInfo is committed (designates a sealing block),
    mark the epoch as sealed and stop it from doing any further work: it no longer verifies
    blocks, builds/proposes blocks, or broadcasts/sends messages.
    
    The motivation is to prevent the epoch from interacting with the VM or other nodes,
    as a sealed epoch is supposed to be terminated right after its sealing.
    
    High level changes:
    
    - Added epochSealed (an atomic.Bool)
    - indexFinalization() now sets epochSealed and returns without progressing to
      the next round when the committed block is a sealing block.
    - broadcast/send now abort if sealed, and the block
      verification and block-building tasks abort when the epoch is sealed.
    
    Add TestEpochStopsAtSealingBlock, which drives an epoch through a chain
    via block messages, finalizations and notarizations and asserts that:
    without a sealing block every block is verified and the epoch proposes a
    block for the round it leads; with a sealing block only the blocks up to
    and including it are verified and committed.
    
    TestEpochBuildsInRoundAfterSealingBlock which drives an epoch through a chain
    via block messages and finalizations and ensures that a block is not built
    in the round after the epoch is sealed.
    
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>
    
    * Broadcast finalization of the sealing block right before sealing the epoch
    
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>
    
    * Allow sending replication messages after epoch is sealed
    
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>
    
    ---------
    
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>

--- common/msg_test.go
+++ common/msg_test.go
@@ -1,3 +1,6 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package common_test
 
 import (
@@ -8,6 +11,96 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestIsReplicationMessage(t *testing.T) {
+	tests := []struct {
+		name     string
+		msg      common.Message
+		expected bool
+	}{
+		{
+			name:     "empty message",
+			msg:      common.Message{},
+			expected: false,
+		},
+		{
+			name:     "replication request",
+			msg:      common.Message{ReplicationRequest: &common.ReplicationRequest{}},
+			expected: true,
+		},
+		{
+			name:     "replication response",
+			msg:      common.Message{ReplicationResponse: &common.ReplicationResponse{}},
+			expected: true,
+		},
+		{
+			name:     "verified replication response",
+			msg:      common.Message{VerifiedReplicationResponse: &common.VerifiedReplicationResponse{}},
+			expected: true,
+		},
+		{
+			name:     "block message",
+			msg:      common.Message{BlockMessage: &common.BlockMessage{}},
+			expected: false,
+		},
+		{
+			name:     "verified block message",
+			msg:      common.Message{VerifiedBlockMessage: &common.VerifiedBlockMessage{}},
+			expected: false,
+		},
+		{
+			name:     "empty notarization",
+			msg:      common.Message{EmptyNotarization: &common.EmptyNotarization{}},
+			expected: false,
+		},
+		{
+			name:     "vote",
+			msg:      common.Message{VoteMessage: &common.Vote{}},
+			expected: false,
+		},
+		{
+			name:     "empty vote",
+			msg:      common.Message{EmptyVoteMessage: &common.EmptyVote{}},
+			expected: false,
+		},
+		{
+			name:     "notarization",
+			msg:      common.Message{Notarization: &common.Notarization{}},
+			expected: false,
+		},
+		{
+			name:     "finalize vote",
+			msg:      common.Message{FinalizeVote: &common.FinalizeVote{}},
+			expected: false,
+		},
+		{
+			name:     "finalization",
+			msg:      common.Message{Finalization: &common.Finalization{}},
+			expected: false,
+		},
+		{
+			// A block digest request is a request but not a replication message.
+			name:     "block digest request",
+			msg:      common.Message{BlockDigestRequest: &common.BlockDigestRequest{}},
+			expected: true,
+		},
+		{
+			// When several fields are set, any replication field makes it a replication message.
+			name: "block message and replication request",
+			msg: common.Message{
+				BlockMessage:       &common.BlockMessage{},
+				ReplicationRequest: &common.ReplicationRequest{},
+			},
+			expected: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			require.Equal(t, test.expected, test.msg.IsReplicationMessage())
+		})
+	}
+}
 
 func TestQuorumRoundMalformed(t *testing.T) {
 	tests := []struct {
