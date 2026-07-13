501f5d17d474171abf899be0db485ed116080862
jadalsmail <jad.smail@avalabs.org>
2026-07-13 17:51:17 -0400

    fix replication requests exceeding maxRoundWindow + splitting resends (#431)
    * fix replication requests exceeding maxRoundWindow and splitting resendRequests
    
    * go format fixed
    
    * extract DistributeMissingSequences and enfoce maxRoundWindow on retried requests
    
    * fix .gitignore rule and formatting
    
    * format
    
    * add batchSequences -- initial and retry replication paths call the same function
    
    * add comments + simplify code to use Slice.chunk
    
    * fix TestReplicationResendSplitsRequests now tests resend reequests above the limit
    
    ---------
    
    Co-authored-by: yacovm <yacovm@users.noreply.github.com>

--- simplex/epoch.go
+++ simplex/epoch.go
@@ -30,6 +30,7 @@ const (
 	DefaultEmptyVoteRebroadcastTimeout    = 5 * time.Second
 	DefaultFinalizeVoteRebroadcastTimeout = 6 * time.Second
 	EmptyVoteTimeoutID                    = "rebroadcast_empty_vote"
+	maxItemCountPerRequest                = 10 // max number of rounds or sequences that fit in one replication request
 )
 
 type EmptyVoteSet struct {
