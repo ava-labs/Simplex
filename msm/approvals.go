309860d72c43daf2f80dc12532a75e96f15ed08f
samliok <samliok@stanford.edu>
2026-07-30 16:43:06 -0400

    cleanup instance

--- msm/approvals.go
+++ msm/approvals.go
@@ -159,7 +159,7 @@ func (as *ApprovalStore) checkApprovalSignature(approval *common.ValidatorSetApp
 }
 
 func (as *ApprovalStore) approvalExistsAndUpToDate(approval *common.ValidatorSetApproval, timestamp uint64) bool {
-	if as.approvalsByNodes[avalanchego.NodeID(approval.NodeID)] == nil {
+	if as.approvalsByNodes[approval.NodeID] == nil {
 		return false
 	}
 
@@ -168,7 +168,7 @@ func (as *ApprovalStore) approvalExistsAndUpToDate(approval *common.ValidatorSet
 		auxInfoDigest: approval.AuxInfoDigest,
 	}
 
-	existingApproval := as.approvalsByNodes[avalanchego.NodeID(approval.NodeID)][key]
+	existingApproval := as.approvalsByNodes[approval.NodeID][key]
 	if existingApproval == nil {
 		return false
 	}
