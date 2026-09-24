9f8a8e57b0d7d56ca4667fade26e85bdad4d4f42
samliok <samliok@stanford.edu>
2026-09-24 12:31:22 -0500

    bound

--- simplex/epoch_test.go
+++ simplex/epoch_test.go
@@ -3484,3 +3484,49 @@ func TestRebroadcastDoesNotFinalizeVoteOnTimedOutRound(t *testing.T) {
 		}
 	}
 }
+
+// TestReplicationLatestSeqBeyondWindowIsNotStored asserts a LatestSeq beyond the round window is not stored.
+func TestReplicationLatestSeqBeyondWindowIsNotStored(t *testing.T) {
+	nodes := []NodeID{{1}, {2}, {3}, {4}}
+	conf, _, storage := testutil.DefaultTestNodeEpochConfig(t, nodes[1], testutil.NewNoopComm(nodes), testutil.NewTestBlockBuilder())
+	conf.ReplicationEnabled = true
+	conf.MaxRoundWindow = 2
+
+	e, err := NewEpoch(conf)
+	require.NoError(t, err)
+	t.Cleanup(e.Stop)
+	require.NoError(t, e.Start())
+
+	sigAggr := e.SignatureAggregatorCreator(conf.Comm.Validators())
+	md := e.Metadata()
+	var chain []QuorumRound
+	for seq := uint64(0); seq <= 3; seq++ {
+		md.Seq = seq
+		md.Round = seq
+		block := testutil.NewTestBlock(md, emptyBlacklist)
+		finalization, _ := testutil.NewFinalizationRecord(t, sigAggr, block, nodes)
+		chain = append(chain, QuorumRound{Block: block, Finalization: &finalization})
+		md.Prev = block.BlockHeader().Digest
+	}
+
+	// farAheadBlock is past the max round window, so it shouldn't be stored in replication state
+	farAheadBlock := testutil.NewTestBlock(chain[3].Block.BlockHeader().ProtocolMetadata, emptyBlacklist)
+	farAheadBlock.Data = []byte("far ahead")
+	farAheadBlock.ComputeDigest()
+	farAheadFinalization, _ := testutil.NewFinalizationRecord(t, sigAggr, farAheadBlock, nodes)
+	require.NoError(t, e.HandleMessage(&Message{ReplicationResponse: &ReplicationResponse{
+		LatestSeq: &QuorumRound{Block: farAheadBlock, Finalization: &farAheadFinalization},
+	}}, nodes[2]))
+
+	require.NoError(t, e.HandleMessage(&Message{ReplicationResponse: &ReplicationResponse{
+		Data: chain[:3],
+	}}, nodes[2]))
+	storage.WaitForBlockCommit(2)
+
+	require.NoError(t, e.HandleMessage(&Message{ReplicationResponse: &ReplicationResponse{
+		Data: chain[3:],
+	}}, nodes[2]))
+	committed := storage.WaitForBlockCommit(3)
+	// Ensure the farAheadBlock was not the one committed
+	require.Equal(t, chain[3].Block.BlockHeader().Digest, committed.BlockHeader().Digest)
+}
